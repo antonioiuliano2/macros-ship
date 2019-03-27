372722e455263f706b53b99387c440ddc1a4c921
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2019-03-27 19:05:18 +0100

    adding variable for number of grains

--- FEDRA/fromFairShip2Fedra.C
+++ FEDRA/fromFairShip2Fedra.C
@@ -1,5 +1,5 @@
 //tool to load hit from emulsion to FEDRA (13 aprile 2018) //updated to work with the new TTreeReader structure (6 March)
-//to use it, go in a directory and create the folder b000001/p001 to b000001/p029
+//to use it, go in a directory and create the folders b000001/p001 to b000001/p029
 //then launch it from the directory mother of b000001
 
 #include <stdio.h>
@@ -11,18 +11,13 @@ using namespace TMath;
 TRandom *grandom = new TRandom3(); //creating every time a TRandom3 is a bad idea
 void smearing (Float_t &TX, Float_t &TY, const float angres);
 bool efficiency(const float emuefficiency);
-void fromFairShip2Fedra(int nplate);
-
-/*void fromFairShip2Fedra(){
-	const int nplates = 29;
-	for (int iplate = 1; iplate <= 29; iplate++){
-		fromFairShip2Fedra(iplate);
-	}
-}*/
 
 void fromFairShip2Fedra(){
  const float emuefficiency = 0.9;
  const float angres = 0.003; // 3 milliradians
+ const float ngrains = 70; //the same number for all the couples, so they have the same weigth.
+ const int nplates = 29;
+ //**********************OPENING INPUT FILE***************************
  TFile * inputfile = TFile::Open(" /eos/experiment/ship/user/aiuliano/SHiP_sim/uniformonespill_onelayertungsten_ch1_07_03_19/pythia8_Geant4_1000_0.5.root");
  //TFile * inputfile = TFile::Open("/eos/user/a/aiuliano/sims_FairShip/sim_charm/pot/uniformonespill_onelayer_ch1_03_03_19/pythia8_Geant4_1000_0.5.root");
  if (!inputfile) return;
@@ -37,22 +32,23 @@ void fromFairShip2Fedra(){
  const Int_t nevents = reader.GetTree()->GetEntries();
  int ihit = 0, ievent = 0;
  int nfilmhit = 0;
- int trackID = 0, motherID = 0;
+ int trackID = 0, motherID = 0, pdgcode = 0;
+ //***********************CREATING FEDRA TREES**************************
  gInterpreter->AddIncludePath("/afs/cern.ch/work/a/aiuliano/public/fedra/include");
-
- const int nplates = 29;
- EdbCouplesTree ect[nplates];
- for (int i = 0; i < nplates; i++){
-  if (nplate <10) ect[i].InitCouplesTree("couples",Form("b000001/p00%i/1.%i.0.0.cp.root",nplate,nplate),"RECREATE");
-  else ect[i].InitCouplesTree("couples",Form("b000001/p0%i/1.%i.0.0.cp.root",nplate,nplate),"RECREATE");
+ EdbCouplesTree *ect[nplates];
+ for (int i = 1; i <= nplates; i++){
+  ect[i-1] = new EdbCouplesTree();
+  if (i <10) ect[i-1]->InitCouplesTree("couples",Form("b000001/p00%i/1.%i.0.0.cp.root",i,i),"RECREATE");
+  else ect[i-1]->InitCouplesTree("couples",Form("b000001/p0%i/1.%i.0.0.cp.root",i,i),"RECREATE");
  }
  Int_t Flag = 1;
    
-   
+ //************************STARTING LOOP ON SIMULATION******************  
  while (reader.Next()){
    for (const BoxPoint& emupoint:emulsionhits){
      bool savehit = true; //by default I save all hits
 //no you don't want to do this//     if (j % 2 == 0) continue;
+     pdgcode = emupoint.PdgCode();
      trackID = emupoint.GetTrackID();
      motherID = tracks[trackID].GetMotherId();
      xem = emupoint.GetX()* 1E+4 + 62500;
@@ -64,26 +60,28 @@ void fromFairShip2Fedra(){
 
      if ((TDatabasePDG::Instance()->GetParticle(pdgcode))!=NULL) charge = TDatabasePDG::Instance()->GetParticle(pdgcode)->Charge();
      else charge = 0.;
-     nfilmhit = emupoint.GetDetectorID()
-     //saving the hits for a plate in the corresponding couples (only one layer saved, the other has ID + 10000)         
-       //Inserting real data effects in the simulation
-     if(!efficiency(emuefficiency)) savehit = false; //inserting some holes due to emulsion inefficiency
+     nfilmhit = emupoint.GetDetectorID(); //getting number of the film
+     //*************EXCLUDE HITS FROM BEING SAVED*******************
+     if (nfilmhit > 1000) savehit = false;
      if(charge == 0.) savehit = false; //we do not track neutral particles
-     smearing(tx,ty,angres);
-     //Saving the hit in FEDRA
+     //saving the hits for a plate in the corresponding couples (only one layer saved, the other has ID + 10000)         
+       //Inserting real data effects in the simulation (now COMMENTED OUT)
+    // if(!efficiency(emuefficiency)) savehit = false; //inserting some holes due to emulsion inefficiency       
+    // smearing(tx,ty,angres);
+     //**************SAVING HIT IN FEDRA BASE-TRACKS****************
      if (savehit){        
-      ect[nfilmhit-1].eS->Set(ihit,xem,yem,tx,ty,1,Flag);
-      ect[nfilmhit-1].eS->SetMC(ievent, trackID); //objects used to store MC true information
-      ect[nfilmhit-1].eS->SetAid(motherID, 0); //forcing areaID member to store mother MC track information
-      ect[nfilmhit-1].eS->SetW(70.); //need a high weight to do tracking
-      ect[nfilmhit-1].Fill();
+      ect[nfilmhit-1]->eS->Set(ihit,xem,yem,tx,ty,1,Flag);
+      ect[nfilmhit-1]->eS->SetMC(ievent, trackID); //objects used to store MC true information
+      ect[nfilmhit-1]->eS->SetAid(motherID, 0); //forcing areaID member to store mother MC track information
+      ect[nfilmhit-1]->eS->SetW(ngrains); //need a high weight to do tracking
+      ect[nfilmhit-1]->Fill();
       ihit++; //hit entry, increasing as the tree is filled        
       }
      }//end of loop on emulsion points
     ievent++;
    } //end of loop on tree
-  for (int iplate = 0; iplate < nplate; iplate++){
-   ect[iplate].Close();  
+  for (int iplate = 0; iplate < nplates; iplate++){
+   ect[iplate]->Close();  
  }
 }
 
