462d90a8cacfcee722386fba3dd4d5468ceb4496
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2019-05-07 17:37:08 +0200

    adding option for inputfile and outputfile names

--- analisi_charmsim/muonrpc_loop.C
+++ analisi_charmsim/muonrpc_loop.C
@@ -1,7 +1,12 @@
 //where high number of electrons are generated?
+/*Usage: root -l
+         .L muonrpc_loop.C
+         muonrpc_loop("/inputpath/ship.conical.MuonBack-TGeant4.root",
+*/
+
 #include <assert.h>     /* assert */
-void muonrpc_loop(){
- TFile *inputfile = TFile::Open("ship.conical.MuonBack-TGeant4.root");
+void muonrpc_loop(TString inputfilename, TString outputfilename="origin_hitsrpc.root"){
+ TFile *inputfile = TFile::Open(inputfilename.Data());
  TTreeReader reader("cbmsim",inputfile); //reading file loaded before executing the script
 
  TTreeReaderArray<ShipMCTrack> tracks(reader,"MCTrack");
@@ -14,35 +19,36 @@ void muonrpc_loop(){
 
  TH1F *hstartz = new TH1F("hstartz", "production position of the electron/positrons registered in RPC station 0",4000,-6000,-2000);
  TH2F *hstartxy = new TH2F("hstartxy", "production position of the electron/positrons registered in RPC station 0", 100,-500,500,100, -500, 500);
- //loop on the events
+ //**********************MAIN LOOP on the events****************************
  const int nevents = (reader.GetTree())->GetEntries();
  cout<<"number of entries: "<<nevents<<endl;
  int ievent = 0;
  while (reader.Next()){
   if (ievent % 100000 == 0) cout<<"now processing event "<<ievent<<" over "<<nevents<<endl;
-  // if (ievent >= 100000) break;
+  
   int irpc = 0; //to keep track of rpcindex
-  //loop on rpc points
+  //**********************loop on rpc points************
   for (const ShipRpcPoint &rpcpoint: rpcpoints){ 
    int trackID = rpcpoint.GetTrackID();
    int hitpdg = rpcpoint.PdgCode();
    int detID = rpcpoint.GetDetectorID();
    int nplane = detID - 10000;
 
-   assert(trackID>0); //we can access track information for this hit
-   int procID = tracks[trackID].GetProcID();
+   assert(trackID>0); //otherwise we cannot access track information for this hit
+   ShipMCTrack mctrack = tracks[trackID];
+   int procID = mctrack.GetProcID();
    //xyz start position of the track
-   Float_t startx = tracks[trackID].GetStartX();
-   Float_t starty = tracks[trackID].GetStartY();
-   Float_t startz = tracks[trackID].GetStartZ();
-   Float_t momentum = tracks[trackID].GetP();
-   Float_t weight = tracks[trackID].GetWeight();   
+   Float_t startx = mctrack.GetStartX();
+   Float_t starty = mctrack.GetStartY();
+   Float_t startz = mctrack.GetStartZ();
+   Float_t momentum = mctrack.GetP();
+   Float_t weight = mctrack.GetWeight();   
    //we can also access information about the mother
-   int motherID = tracks[trackID].GetMotherId();
-   Float_t mothermomentum = tracks[motherID].GetP();
-   int motherpdgcode = tracks[motherID].GetPdgCode();
-   if ((nplane == 0) && (TMath::Abs(hitpdg) == 11)){ //CUT for histogram filling (electrons in RPC number 0
-    cout<<"rpcpos: "<<rpcpoint.GetX()<<" "<<rpcpoint.GetY()<<" "<<rpcpoint.GetZ()<<endl;
+   ShipMCTrack mothermctrack = tracks[mctrack.GetMotherId()];
+   Float_t mothermomentum = mothermctrack.GetP();
+   int motherpdgcode = mothermctrack.GetPdgCode();
+   //***********************************CUT for histogram filling (electrons in RPC number 0) *****************
+   if ((nplane == 0) && (TMath::Abs(hitpdg) == 11)){   
     hprocessID->Fill(procID,weight);
     hstartxy->Fill(startx, starty,weight);
     hstartz->Fill(startz,weight);
@@ -55,7 +61,7 @@ void muonrpc_loop(){
  ievent++;
  } //end of the loop over the vents
  inputfile->Close();
- //**********************drawing of the histograms
+ //**********************drawing of the histograms****************************************
  TCanvas *cstartpos = new TCanvas();
  cstartpos->Divide(1,2);
  cstartpos->cd(1);
