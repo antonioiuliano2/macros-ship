a827b69de706dee5815c8297b36b9cbee21ee2d4
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2019-06-13 17:11:07 +0200

    first production version ready

--- bash/unpackall.sh
+++ bash/unpackall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #environment definition
-DATADIR=/eos/experiment/ship/data/charmxsec/DATA_8000_charmTest/
-ONLINEFOLDER=/afs/cern.ch/work/a/aiuliano/public/SHIPBuild/oliver_online/
+DATADIR=/eos/experiment/ship/data/charmxsec/DATA_8000_charmTest
+ONLINEFOLDER=/afs/cern.ch/work/a/aiuliano/public/SHIPBuild/oliver_online
 
 #loop on folders
 
@@ -13,10 +13,14 @@ for RUNFOLDER in $(ls $DATADIR/rawdata);
  for FILENAME in $(ls $DATADIR/rawdata/$RUNFOLDER);
   do
    NOEXTENSIONFILENAME=${FILENAME/.raw/}
-   echo "unpacking file "$FILENAME "into "  $NOEXTENSIONFILENAME.root
-   #note, if outputfile already exists, will be recreated by FairRoot->be careful to check if the paths are set correctly, expecially for outputfiles!
-   #$ONLINEFOLDER/unpack.py -f $DATADIR/rawdata/RUN_8000_$1/$FILENAME -n $1 --   charm -o $DATADIR/rootdata/RUN_8000_$1/$NOEXTENSIONFILENAME.root 
-   fi
+
+   INPUTFILE=$DATADIR/rawdata/RUN_8000_$RUNNUMBER/$FILENAME
+   OUTPUTFILE=$DATADIR/rootdata/RUN_8000_$RUNNUMBER/$NOEXTENSIONFILENAME.root 
+
+   echo "unpacking file "$INPUTFILE
+   echo "into "  $OUTPUTFILE
+   #note, if outputfile already exists, will be recreated by FairRoot->be careful to check if the paths are set correctly before launching, expecially for outputfiles!
+   $ONLINEFOLDER/unpack.py -f $INPUTFILE -n $RUNNUMBER --charm -o $OUTPUTFILE
   done
 
  done
