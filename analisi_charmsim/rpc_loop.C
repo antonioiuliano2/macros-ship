4d4acfe630ff58ff4100452c4ae609ba09870da4
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2019-07-02 17:32:28 +0200

    adding weight information

--- analisi_charmsim/rpc_loop.C
+++ analisi_charmsim/rpc_loop.C
@@ -1,21 +1,28 @@
 /*simple script to loop over events, MC tracks and hit points
 Works without any include, provided that we are in FairShip environment,
 just launch root -l simple_loop in the folder with the simulation output*/
-void rpc_loop(inputfile){
 
- TFile *file = TFile::Open(inputfile); 
+void rpc_loop(TString *inputfile);
+
+void rpc_loop(TString *inputfile){
+
+ TFile *file = TFile::Open(inputfile->Data()); 
  if (!file) return;
  TTreeReader reader("cbmsim",file);
 
  TTreeReaderArray<ShipMCTrack> tracks(reader,"MCTrack");
- TTreeReaderArray<PixelModulesPoint> rpcpoints(reader,"ShipRpcPoint");
+ TTreeReaderArray<ShipRpcPoint> rpcpoints(reader,"ShipRpcPoint");
 
-// cout<<"Number of events"<<reader.GetEntries()<<endl;
+ //cout<<"Number of events"<<reader.GetEntries()<<endl;
  int ientry = 0;
  while (reader.Next()){
      ientry = reader.GetCurrentEntry();// keeps track of the number of event (from 0 to Nevents - 1)    
-     //access the hits:    
-     for (const ShipRpcPoint& rpcpoint: rpcpoints){
+     //getting weight information
+     ShipMCTrack firsttrack = tracks[0]; //weight is the same for all the tracks of the same event
+     double weight = firsttrack.GetWeight(); //weight defined as sum (x_i pho_i)
+
+     //access the hits:         
+     for (const ShipRpcPoint& rpcpoint: rpcpoints){       
          double x = rpcpoint.GetX();
          double y = rpcpoint.GetY();
          double z = rpcpoint.GetZ();
@@ -31,10 +38,15 @@ void rpc_loop(inputfile){
          if (interestingtrack){
            if (trackID < 0) cout<<"cannot show track, too low kinetic energy" <<endl;
             else{
-             ShipMCTrack *mytrack = tracks[trackID];
-             int procID = mytrack->GetProcID();
+             ShipMCTrack mytrack = tracks[trackID];
+             int procID = mytrack.GetProcID();
            }
          }
      }
  } 
+}
+
+void rpc_loop(){
+ TString *inputfilename = new TString("/eos/user/a/aiuliano/public/sims_FairShip/sim_nutau/neutrinos2019/8/nu_e/ship.conical.Genie-TGeant4.root");
+ rpc_loop(inputfilename);
 }
