f00cf8248f79a8bb3cba276b6b506e5d37773f49
AntonioIuliano <antonio.iuliano@cern.ch>
2019-08-27 18:15:34 +0200

    updating code

--- FEDRA/drawEDAvertex.C
+++ FEDRA/drawEDAvertex.C
@@ -1,6 +1,7 @@
 void drawEDAvertex(int vID = 0){
  const int nvertices = 1;
- int vertexlist[nvertices] = {3781};
+ int vertexlist[nvertices] = {2947};
+ int trackID = 166641;
 
  TFile * inputfile = TFile::Open("vertices_firstquarter.root");
  EdbVertexRec *vertexrec = (EdbVertexRec*) inputfile->Get("EdbVertexRec");
@@ -20,11 +21,58 @@ void drawEDAvertex(int vID = 0){
      drawntracks->Add(track);
   }
  }
- EdbPVRec *pvr = new EdbPVRec();
- pvr->eTracks = drawntracks;
- pvr->eVTX = drawnvertices;
+ EdbPVRec *ali = new EdbPVRec();
 
- EdbEDA * eda = new EdbEDA(pvr); // init DataSet but doesn't read linked_track.root
+  //reading track file and setting branches
+  TFile *trackfile = TFile::Open("verticesandtracks.root");
+  TTree *tracks = (TTree*)trackfile->Get("tracks");
+
+  Int_t   trid=0;
+  Int_t   nseg=0;
+  Int_t   npl=0;
+  Int_t   n0=0;
+  Float_t xv=0.;
+  Float_t yv=0.;
+
+  TClonesArray *seg  = new TClonesArray("EdbSegP", 60);
+  TClonesArray *segf = new TClonesArray("EdbSegP", 60);
+  EdbSegP *trk=0;
+  EdbSegP *s1=0;
+
+  tracks->SetBranchAddress("trid", &trid);
+  tracks->SetBranchAddress("nseg", &nseg);
+  tracks->SetBranchAddress("npl", &npl);
+  tracks->SetBranchAddress("n0", &n0);
+  tracks->SetBranchAddress("xv", &xv);
+  tracks->SetBranchAddress("yv", &yv);
+  tracks->SetBranchAddress("sf", &segf);
+  tracks->SetBranchAddress("s",  &seg);
+  tracks->SetBranchAddress("t.", &trk);
+
+  //Getting Entry
+
+  tracks->BuildIndex("trid");
+  tracks->GetEntryWithIndex(trackID);
+
+  EdbTrackP *tr1 = new EdbTrackP();
+  ((EdbSegP*)tr1)->Copy(*trk);
+  tr1->SetM(0.139);                 //TODO
+
+  for(int i=0; i<nseg; i++) {
+      s1 = (EdbSegP*)(seg->At(i));
+
+      tr1->AddSegment( s1 );
+      tr1->AddSegmentF( new EdbSegP(*((EdbSegP*)(segf->At(i)))) );
+    }
+    tr1->SetSegmentsTrack(tr1->ID());
+    tr1->SetCounters();
+    //tr1->FitTrackKFS(true);
+
+ //adding track to drawing set and building EDA
+ cout<<"Test: "<<tr1->N()<<endl;
+ drawntracks->Add(tr1);
+ ali->eTracks = drawntracks;
+ ali->eVTX = drawnvertices;
+ EdbEDA * eda = new EdbEDA(ali); // init DataSet but doesn't read linked_track.root
  eda->Run();
-
 }
