d4aedde291550d15ec5500eb707889ec3333b45f
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2019-10-24 00:03:11 +0200

    adding track info for Chris

--- FEDRA/extracttracks.py
+++ FEDRA/extracttracks.py
@@ -9,19 +9,31 @@ inputtree.SetAlias("t","t.")
 
 nentries = inputtree.GetEntries()
 tracks = ROOT.TClonesArray("EdbSegP") #try with a TClonesArray
+tracks_COV = ROOT.TClonesArray("TMatrixD")
+
 copiedseg = ROOT.EdbSegP()
 inputtree.Branch("track",tracks) 
+
+inputtree.Branch("track_COV",tracks_COV)
 #we add a branch with the copied track as EdbSegP, to solve reading issue
 print ("Starting loop over {} events".format(nentries))
 for ientry in range(nentries):
- tracks.Clear()
+ #clearing TClonesArrays
+ tracks.Clear("C")
+ tracks_COV.Clear("C")
+
+
  copiedseg = ROOT.EdbSegP()
  inputtree.GetEntry(ientry)
  track = inputtree.t
-
+ #getting covariance matrix and adding it to the TClonesArray
+ covtrack = track.COV()
+ tracks_COV[0] = covtrack
+ #copying track branch into a TClonesArray of EdbSegP
  #copiedseg.Copy(track)
  copiedseg = ROOT.EdbSegP(track)
  tracks[0] = copiedseg
+
  inputtree.Fill()
  if ((ientry % 100000) == 0) :
   print ("arrived at entry ",ientry)
