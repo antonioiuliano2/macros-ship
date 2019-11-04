6ce0d68b89397c7265eb75dae61b62696d25099c
AntonioIuliano <antonio.iuliano@cern.ch>
2019-11-04 10:05:13 +0100

    first draft of updated counting MC script

--- analisi_charmsim/countvertextopologies.py
+++ analisi_charmsim/countvertextopologies.py
@@ -4,11 +4,14 @@ import pandas as pd
 
 dfall = pd.read_csv('MC_vertexlist_testremainingtracks.csv')
 
+dfall = dfall.sort_values(["MCEventID","topology","ntracks"])
 #removing duplicates of same track
 
 df = dfall.drop_duplicates(subset=["MCEventID","MCTrackID","MCMotherID"]) #many tracks are splitted up, first instance is kept
 
-dfcharm = df.groupby(['MCEventID','MCMotherID']).first()
+dfcharm = df[df['topology']>1.5].groupby(['MCEventID','MCMotherID']).first()
+
+print ("How many charm daughters in log?", len(dfcharm))
 
 dfconnected = dfcharm[dfcharm['topology'] > 2.5]
 
@@ -18,6 +21,33 @@ dfextra = dfcharm[dfcharm['topology'] > 3.5]
 
 print ("How many extra? ",len(dfextra))
 
-testpairconnected = dfconnected.groupby(['MCEventID']).sum()
-print (len(testpairconnected))
-print (len(testpairconnected[testpairconnected['quantity']==2]))
+#counting for pair
+pairconnected = dfconnected.groupby(['MCEventID']).sum()
+print ("Single charm daughter connected to parent: ",len(pairconnected))
+print ("Both charm daughters connected to parent: ",len(pairconnected[pairconnected['quantity']==2]))
+
+pairextra = dfextra.groupby(['MCEventID']).sum()
+print ("Single charm daughter connected to parent: ",len(pairextra))
+print ("Both charm daughters connected to parent: ",len(pairextra[pairextra['quantity']==2]))
+
+#vertex reconstruction (1 primary, 2 secondary)
+dfvertices = dfcharm[dfcharm['topology']<=2.5]
+dfvertices = dfvertices[dfvertices['topology']>=1.5]
+#dfvertices.groupby(['MCEventID','ivtx']).sum()
+# Double Signal: topology 1, 2 ,2 total 5
+# Single Signal: topology 1,2 or 2,2: total less than 5
+
+#Quick check, molteplicity (TO DO; uso topology)
+dfprimary = dfvertices[dfvertices['ntracks']>=6]
+print ("How many associated to primary",len(dfprimary))
+dfsecondary = dfvertices[dfvertices['ntracks']<6]
+print ("How many associated to secondary",len(dfsecondary))
+
+#counting for pair
+pairprimary = dfprimary.groupby(['MCEventID']).sum()
+print ("Single charm daughter connected to primary: ",len(pairprimary))
+print ("Both charm daughters connected to primary: ",len(pairprimary[pairprimary['quantity']==2]))
+
+pairsecondary = dfsecondary.groupby(['MCEventID']).sum()
+print ("Single charm daughter connected to secondary: ",len(pairsecondary))
+print ("Both charm daughters connected to secondary: ",len(pairsecondary[pairsecondary['quantity']==2]))
