007c3a0de17ffb7c25ff7d8371f30c461e7d6f09
AntonioIuliano <antonio.iuliano@cern.ch>
2019-11-25 17:35:56 +0100

    updating elog for FOOT scans

--- analisi_charmdata/elog_export.py
+++ analisi_charmdata/elog_export.py
@@ -11,18 +11,18 @@ homepath = os.environ['HOME']
 
 register_matplotlib_converters() #for datetimes
 
-df = pd.read_excel(homepath+"/Dropbox/Archivio_cronologico/Giugno_2019/elog_export_shipcharm_21_giugno.xlsx")
-
+#df = pd.read_excel(homepath+"/Dropbox/Archivio_cronologico/Giugno_2019/elog_export_shipcharm_21_giugno.xlsx")
+df = pd.read_csv("GSIscans_22_11_19.csv")
 # ROOT histograms and Canvases are built outside functions, otherwise they are killed when function ends
 croot = ROOT.TCanvas()
 
-hbaseslavich = ROOT.TH1D("hbaseslavich", "Base plastic from Slavich emulsions", 13, 147.5, 212.5)
-hbasenagoya = ROOT.TH1D("hbasenagoya", "Base plastic from Nagoya emulsions", 13, 147.5, 212.5)
+hbaseslavich = ROOT.TH1D("hbaseslavich", "Base plastic from Slavich emulsions;thickness[#mum]", 14, 147.5, 217.5)
+hbasenagoya = ROOT.TH1D("hbasenagoya", "Base plastic from Nagoya emulsions;thickness[#mum]", 14, 147.5, 217.5)
 
 croot1 = ROOT.TCanvas()
 
-hemuslavich = ROOT.TH1D("hemuslavich", "Slavich emulsions", 14, 45, 115)
-hemunagoya = ROOT.TH1D("hemunagoya", "Nagoya emulsions", 14, 45, 115)
+hemuslavich = ROOT.TH1D("hemuslavich", "Slavich emulsions;thickness[#mum]", 20, 45, 145)
+hemunagoya = ROOT.TH1D("hemunagoya", "Nagoya emulsions;thickness[#mum]", 20, 45, 145)
 
 def measuredthicknesses():
  ''' plot thicknesses of plastic base and emulsion layer '''
@@ -33,11 +33,10 @@ def measuredthicknesses():
  fill_hist(hbaseslavich, dfslavich['Base'])
  fill_hist(hbasenagoya, dfnagoya['Base'])
 
- hbaseslavich.Draw()
- hbaseslavich.GetXaxis().SetTitle("thickness[#mum]")
- hbasenagoya.SetLineColor(ROOT.kRed)
- hbasenagoya.Draw("SAMES")
 
+ hbasenagoya.SetLineColor(ROOT.kRed)
+ hbasenagoya.Draw()
+ hbaseslavich.Draw("SAMES")
  croot.BuildLegend() # I trust you will build it correctly
 
  croot1.cd()
@@ -48,11 +47,9 @@ def measuredthicknesses():
  fill_hist(hemuslavich, emuthickslavich)
  fill_hist(hemunagoya, emuthicknagoya)
 
- hemuslavich.Draw()
- hemuslavich.GetXaxis().SetTitle("thickness[#mum]")
  hemunagoya.SetLineColor(ROOT.kRed)
- hemunagoya.Draw("SAMES")
-
+ hemunagoya.Draw()
+ hemuslavich.Draw("SAMES")
  croot1.BuildLegend() # I trust you will build it correctly
  '''Leaving this commented part as an example of how to do hist plots with matplotlib'''
  #fig1 = plt.figure()
@@ -65,7 +62,7 @@ def measuredthicknesses():
 
 def scanningtime():
  from matplotlib import pyplot as plt #ROOT Canvases do not like matplotlib
- '''check number of plate scanned per mic in each microscope'''
+ '''check number of plate scanned per mic in each microscope, need to edit time format with Excel beforehand'''
  #getting lists
  df.index = pd.to_datetime(df['Date'])
  dfnomic5 = df.query('Microscope != "mic5"')
@@ -97,36 +94,69 @@ def scanningtime():
 def shifters():
  from matplotlib import pyplot as plt #ROOT Canvases do not like matplotlib
  '''reporting the shifter hard work'''
- nAntonio = df['Author'].str.contains('Iuliano').value_counts()[True]
- nAntonia = df['Author'].str.contains('Crescenzo').value_counts()[True]
- nValerio = df['Author'].str.contains('Gentile').value_counts()[True]
- nGiuliana = df['Author'].str.contains('Galati').value_counts()[True]
- nAdele = df['Author'].str.contains('Lauria').value_counts()[True]
- nCristina = df['Author'].str.contains('Montesi').value_counts()[True]
- nBrenda = df['Author'].str.contains('Capone').value_counts()[True]
- nArtem = df['Author'].str.contains('Golovatiuk').value_counts()[True]
-
- ntotal = df.count().tolist()[0]
-
+ Authors = ['Emil','Iuliano','Crescenzo','Gentile','Galati','Lauria','Montesi','Capone','Golovatiuk'] #as inserted by shifters, not whole name needs to be inserted
+ labels = ['Emil','Antonio', 'Antonia','Valerio','Giuliana','Adele','Cristina','Brenda','Artem'] #names to be displayed in the plot
+ colors = ['b','g','m', 'y','r','b','c','r','m'] 
  ratios = [] #percentages to fill the pie plot
- ratios.append(float(nAntonio)/ntotal*100)
- ratios.append(float(nAntonia)/ntotal*100)
- ratios.append(float(nValerio)/ntotal*100)
- ratios.append(float(nGiuliana)/ntotal*100)
- ratios.append(float(nAdele)/ntotal*100)
- ratios.append(float(nCristina)/ntotal*100)
- ratios.append(float(nBrenda)/ntotal*100)
- ratios.append(float(nArtem)/ntotal*100)
+ ntotal = len(df)
 
- print ntotal
- print ratios
+ for author in Authors:
+     nscans = df['Author'].str.contains(author).value_counts()[True] 
+     ratios.append(float(nscans)/ntotal*100)
 
- #explode = [0., 0,] #we want to 'extract' the slice of already scanned emulsions 
- colors = ['g','m', 'y','r','b','c','r','m'] 
- labels = ['Antonio', 'Antonia','Valerio','Giuliana','Adele','Cristina','Brenda','Artem']
+ #sanity check
+ if (sum(ratios)<100):
+     print ("Warning: sum of scanning percentage lower than 100%: maybe missing some shifters?")
+ elif (sum(ratios)>100):
+     print ("Error: sum of scanning percentage higher than 100%: this is weird, stopping execution")
+     return 2
+
+ if (len(labels) != len(Authors)):
+     print ("Error: size of labels of pie chart does not match size of shifter list")
+     return 1
+ if (len(colors) != len(Authors)):
+     print ("Error: size of colors of pie chart does not match size of shifter list")
+     return 1
+
  fig1,ax1 = plt.subplots()
  ax1.pie(ratios, colors = colors, autopct='%1.1f%%', labels=labels, shadow=True, startangle=90)
 
  #plt.pie(ratios, explode=explode, colors = colors, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
  ax1.axis('equal') #ensures that the pie is drawn as a circle
  fig1.show()
+
+def processing():
+    '''checking how many plates have been processed and linked'''
+    dfGSI1 = df[df['Scanning Path'].str.contains("GSI1")]
+    dfGSI2 = df[df['Scanning Path'].str.contains("GSI2")]
+    dfGSI3 = df[df['Scanning Path'].str.contains("GSI3")]
+    #removing RE-SCANS and taking only one value
+    print(dfGSI2.sort_values("PlateID"))
+    print("Scanned a total of {} films from GSI1, {} films from GSI2 and {} films from GSI3".format(len(dfGSI1),len(dfGSI2),len(dfGSI3)))
+    dfGSI1 = dfGSI1[dfGSI1["Scanning"]!="TO RE-SCAN"]
+    dfGSI2 = dfGSI2[dfGSI2["Scanning"]!="TO RE-SCAN"]
+    dfGSI3 = dfGSI3[dfGSI3["Scanning"]!="TO RE-SCAN"]
+  
+    dfGSI1 = dfGSI1.drop_duplicates(subset=["PlateID"]) 
+    dfGSI2 = dfGSI2.drop_duplicates(subset=["PlateID"]) 
+    dfGSI3 = dfGSI3.drop_duplicates(subset=["PlateID"]) 
+    print(dfGSI2.sort_values("PlateID"))
+    print("Excluding duplicates: {} films from GSI1, {} films from GSI2 and {} films from GSI3".format(len(dfGSI1),len(dfGSI2),len(dfGSI3)))
+
+    #plotting linking and alignment vs plateID
+
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.plot(dfGSI1["PlateID"],dfGSI1["Processing"],"b*",label = "GSI1")
+    plt.plot(dfGSI2["PlateID"],dfGSI2["Processing"],"r*",label = "GSI2")
+    plt.xlabel("PlateID")
+    plt.ylabel("processing")
+    plt.legend()
+
+    plt.figure()
+    plt.plot(dfGSI1["PlateID"],dfGSI1["Linking"],"b*",label = "GSI1")
+    plt.plot(dfGSI2["PlateID"],dfGSI2["Linking"],"r*",label = "GSI2")
+    plt.xlabel("PlateID")
+    plt.ylabel("Linking")
+    plt.legend()
+    plt.show()
