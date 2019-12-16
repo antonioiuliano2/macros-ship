e64dcd55d08653d1fc2e1ab83cc25cd3752ecc15
aiuliano <aiuliano@cern.ch>
2019-12-16 21:40:49 +0100

    adding angular residuals

--- FEDRA/global_check_align.C
+++ FEDRA/global_check_align.C
@@ -1,169 +1,26 @@
-//comparing the dz after alignment for a brick with tungsten and one with lead, both use Slavich emulsions
+/*
+Checking alignment performances. Modified for DESY2019 bricks format in 16/12/2019
 
-TFile *outputfile = new TFile("alignchecks.root","RECREATE");
-
-void align_check(TString runname, int lastplate=29, int firstplate=1);
-void align_check(TString runname, int lastplate, int firstplate){
- //TString run = "CH1-R6";
- TString path = TString("/eos/user/a/aiuliano/public/Emulsion_SHiPCharm/") + runname +TString("/b000001/");
- //TString path = "/ship/CHARM2018/" + runname +"/b000001/"; 
- TFile *inputfile;
- TCanvas *c = new TCanvas((runname+TString("_dz_phi_coarse")).Data());
- c->SetTitle((runname+TString("_dz_phi_coarse")).Data());
- if (lastplate>29)c->Divide(7,8);
- else c->Divide(5,6);
-
- TCanvas *cxy = new TCanvas((runname+TString("_dxy_final")).Data());
- cxy->SetTitle((runname+TString("_dxy_final")).Data());
- if (lastplate>29)cxy->Divide(7,8);
- else cxy->Divide(5,6);
-
- TH2F *hzphi[maxplates];
- TH2F *hxy[maxplates];
-
- TGraph *peakgraph = new TGraph();
- peakgraph->SetName("peakgraph");
- TGraph *meangraph = new TGraph();
- meangraph->SetName("meangraph");
- TGraph *noisegraph = new TGraph();
- noisegraph->SetName("noisegraph");
-
- EdbPeak2 *combinations;
-
- TH1F *hdzA = new TH1F((TString("hdzA")+runname).Data(),(TString("Dz obtained after alignment for brick ")+runname).Data(),20,1200,1400);
-
- TGraph *hgraphA = new TGraph();
- hgraphA->SetName("hgraph");
-
- float dzA;
- int xbin, ybin,zbin;
-
- //const int lastplate= 57;
- for (int i = firstplate; i <= lastplate; i++){
-  if (i == firstplate) continue;
-  c->cd(i-1);
-  //opening the file with the report
-  inputfile = TFile::Open(Form((path+"AFF/1.%i.0.0_1.%i.0.0.al.root").Data(),i,i-1));
-  if (inputfile){ //check if file is present
-   if (inputfile->GetListOfKeys()->Contains("zphi_coarse") ){ //check if histogram is present (i.e. alignment was not interrupted leading to a zombie-like file, even if not seen as zombie)
-   //zphi histogram
-   hzphi[i-2] = (TH2F*)inputfile->Get("zphi_coarse");
-   hzphi[i-2]->SetTitle(Form("plate %i to plate %i",i,i-1));
-   hzphi[i-2]->Draw("COLZ");   
-
-   //finding the peak in z
-   hzphi[i-2]->GetBinXYZ(hzphi[i-2]->GetMaximumBin(),xbin,ybin,zbin);
-   dzA = TMath::Abs(hzphi[i-2]->GetYaxis()->GetBinCenter(ybin));
-   hdzA->Fill(dzA);
-   hgraphA->SetPoint(i,(i+i+1)/2.,dzA);
-  }
-  if (inputfile->GetListOfKeys()->Contains("peak2c") ){
-  combinations = (EdbPeak2*) inputfile->Get("peak2c");
-  cout<<"Combinations between plates: "<<i<<"and "<<i+1<<endl;
-  combinations->Print();
-  peakgraph->SetPoint(i,(i+i+1)/2.,combinations->Peak(0));
-  meangraph->SetPoint(i,(i+i+1)/2.,combinations->Mean3(0));
-  noisegraph->SetPoint(i,(i+i+1)/2.,combinations->Mean(0));
-}
-  //xy histogram
-  cxy->cd(i-1);
-  if (inputfile->GetListOfKeys()->Contains("xy_final") ){
-  hxy[i-2] = (TH2F*)inputfile->Get("xy_final");
-  hxy[i-2]->SetTitle(Form("plate %i to plate %i",i,i-1));
-  hxy[i-2]->Draw("COLZ");
-  }
-
-  }
- }
-  outputfile->mkdir(runname.Data());
-  outputfile->cd(runname.Data()); //saving canvas in output root file
-
-  c->Write();
-  cxy->Write();
-  TCanvas *cdz = new TCanvas((runname+TString("dz")).Data());
-  cdz->Divide(1,2);
-  cdz->cd(1);
-  hdzA->Draw();
-  hdzA->GetXaxis()->SetTitle("dZ[#mum]");
-  cdz->cd(2);
-  hgraphA->SetTitle("Dz obtained after alignment for brick"+runname);
-  hgraphA->Draw("AP*");
-  hgraphA->GetXaxis()->SetTitle("nplate");
-  hgraphA->GetYaxis()->SetTitle("dZ[#mum]");
-  cdz->Write();
-
-  TCanvas *ccomb = new TCanvas((runname+TString("ccomb")).Data());
-  peakgraph->SetTitle("Study of combinations");
-  peakgraph->SetMarkerColor(kRed);
-  peakgraph->Draw("AP*");
-  meangraph->SetMarkerColor(kYellow);
-  meangraph->Draw("P*SAME");
-  noisegraph->Draw("P*SAME");
-  peakgraph->GetXaxis()->SetTitle("IDPlate");
-  //adding the legend
-  TLegend *legend = new TLegend(0.1,0.7,0.48,0.9);
-  legend->AddEntry("peakgraph","Main peak of aligned couples","lp");
-  //legend->AddEntry("meangraph","Mean in a 3 X 3 region","lp");
-  legend->AddEntry("noisegraph","Average of random combinations","lp");
-  legend->Draw();
-
-  ccomb->Write();
-  outputfile->cd("..");
-}
-
-void global_check_align(){
-
- const int nalignedbricks = 17;
+*/
+void align_check(TString runname, int lastplate=57, int firstplate=1);
+//checks dz between plates after alignment
+void checkdz(TString runname, int lastplate=57, int firstplate=1){
+ //syntax for brickname is 002, 022, 111
+ TString dir = TString("/ship/DESY2019/");
  
- TString alignedbricks[nalignedbricks];
+ TString bricknumber = TString::Format("%d",TString(runname(3,3)).Atoi());
+ cout<<bricknumber<<endl;
 
- alignedbricks[0] = TString("CH1-R1");
- alignedbricks[1] = TString("CH1-R2");
- alignedbricks[2] = TString("CH1-R4");
- alignedbricks[3] = TString("CH1-R6");
+ TString setposition = TString("/b00000"+bricknumber+"/b00000"+bricknumber+".0.0.0.set.root"); //example: /b000005/b000005.0.0.0.set.root
 
- alignedbricks[4] = TString("CH2-R1");
- alignedbricks[5] = TString("CH2-R3");
- alignedbricks[6] = TString("CH2-R4");
- alignedbricks[7] = TString("CH2-R5");
- alignedbricks[8] = TString("CH2-R6");
-
- alignedbricks[9] = TString("CH3-R2");
- alignedbricks[10] = TString("CH4-R2");
- alignedbricks[11] = TString("CH5-R1");
- alignedbricks[12] = TString("CH5-R2");
- alignedbricks[13] = TString("CH5-R3");
- alignedbricks[14] = TString("CH6-R1");
- alignedbricks[15] = TString("CH6-R2");
- alignedbricks[16] = TString("CH6-R3");
-
- for (int i=0; i < nalignedbricks; i++){
-
-  if (i < 9) align_check(alignedbricks[i],29);
-  else align_check(alignedbricks[i],57);
- }
-
-}
-
-
-void checkdzold(TString runname, int nplates=29, int firstplate=1){
- //TFile *file = new TFile((runname+TString(" quicksave.root")).Data(),"RECREATE");
- TString dir = TString("/ship/CHARM2018/");
-
- TString brickA = runname;
-
- TString setposition = TString("/b000001/b000001.0.0.0.set.root");
-
- TH1F *hdzA = new TH1F("hdzA",(TString("Dz obtained after alignment for brick ")+brickA).Data(),20,1200,1400);
+ TH1F *hdzA = new TH1F("hdzA",(TString("Dz obtained after alignment for brick ")+runname).Data(),300,0,3000);
 
  TGraph *hgraphA = new TGraph();
  hgraphA->SetName("hgraph");
 
-
- TFile *fileA = TFile::Open((dir+brickA+setposition).Data());
+ TFile *fileA = TFile::Open((dir+runname+setposition).Data());
  EdbScanSet *setA = (EdbScanSet*) fileA->Get("set");
 
- const int lastplate = nplates;
  float dzA;
  //loop on plates
  for (int i = firstplate; i< lastplate; i++){
@@ -181,13 +38,96 @@ void checkdzold(TString runname, int nplates=29, int firstplate=1){
  hdzA->Draw();
  hdzA->GetXaxis()->SetTitle("dZ[#mum]");
  cdz->cd(2);
- hgraphA->SetTitle("Dz obtained after alignment for brick"+brickA);
+ hgraphA->SetTitle("Dz obtained after alignment for brick")+runname;
  hgraphA->Draw("AP*");
  hgraphA->GetXaxis()->SetTitle("nplate");
  hgraphA->GetYaxis()->SetTitle("dZ[#mum]");
 
- //drawing two canvas with all the alignment checks
- align_check(brickA,nplates);
- //file->cd("");
- //hgraphA->Write();
+}
+
+//check alignment plots (currently xy and zphi)
+
+void align_check(TString runname,int lastplate, int firstplate){
+ TString dir = TString("/ship/DESY2019/");
+ TString bricknumber = TString::Format("%d",TString(runname(3,3)).Atoi());
+ TString path = "/ship/DESY2019/" + runname +"/b00000"+bricknumber+"/"; 
+ TFile *inputfile;
+ //when bricks are made of many plates, needed more than 1 canvas
+ const int nplates = lastplate - firstplate;
+ const int canvasdimx = 4;
+ const int canvasdimy = 4;
+ const int canvasarea = canvasdimx * canvasdimy;
+ //how many canvases do I need?
+ const int ncanvases=nplates/canvasarea+1; 
+ TCanvas *c[ncanvases];
+ TCanvas *cxy[ncanvases];
+ TCanvas *cTXTY[ncanvases];
+ for (int icanvas = 0; icanvas < ncanvases; icanvas++){
+ c[icanvas] = new TCanvas((runname+TString::Format("_dz_phi_coarse_%d",icanvas)).Data());
+ c[icanvas]->SetTitle((runname+TString::Format("_dz_phi_coarse_%d",icanvas)).Data());
+ c[icanvas]->Divide(canvasdimx,canvasdimy); //NEED TO OPEN OTHER CANVASES WHEN PLATES INCREASE
+
+ cxy[icanvas] = new TCanvas((runname+TString::Format("_dxy_final_%d",icanvas)).Data());
+ cxy[icanvas]->SetTitle((runname+TString::Format("_dxy_final_%d",icanvas)).Data());
+ cxy[icanvas]->Divide(canvasdimx,canvasdimy);
+
+ cTXTY[icanvas] = new TCanvas((runname+TString::Format("_dtxty_final_%d",icanvas)).Data());
+ cTXTY[icanvas]->SetTitle((runname+TString::Format("_dtxty_final_%d",icanvas)).Data());
+ cTXTY[icanvas]->Divide(canvasdimx,canvasdimy);
+ }
+
+ TH2F *hzphi[nplates];
+ TH2F *hxy[nplates];
+ TH2F *hTXTY[nplates];
+
+ //const int lastplate= 57;
+ int iplate = 0;
+ int whichcanvas = 0;
+ for (int i = firstplate; i <= lastplate; i++){
+  if (i == firstplate) continue;
+  //computing in which canvas and subcanvas plot the histograms
+  whichcanvas = iplate/canvasarea;
+  c[whichcanvas]->cd(iplate-whichcanvas*canvasarea+1);
+  //opening the file with the report
+  inputfile = TFile::Open(Form((path+"AFF/"+bricknumber+".%i.0.0_"+bricknumber+".%i.0.0.al.root").Data(),i,i-1)); //example:AFF/2.10.0.0_2.9.0.0.al.root
+  if (inputfile){ //check if file is present
+  //zphi plot
+   if (inputfile->GetListOfKeys()->Contains("zphi_coarse") ){ //check if histogram is present (i.e. alignment was not interrupted leading to a zombie-like file, even if not seen as zombie)
+   hzphi[iplate] = (TH2F*)inputfile->Get("zphi_coarse");
+   hzphi[iplate]->SetTitle(Form("plate %i to plate %i;dphi[rad];dz[#mum]",i,i-1));
+   hzphi[iplate]->Draw("COLZ");   
+  }
+  //xy residuals
+  cxy[whichcanvas]->cd(iplate-whichcanvas*canvasarea+1);
+  if (inputfile->GetListOfKeys()->Contains("xy_final") ){
+  hxy[iplate] = (TH2F*)inputfile->Get("xy_final");
+  hxy[iplate]->SetTitle(Form("plate %i to plate %i;dx[#mum];dy[#mum]",i,i-1));
+  hxy[iplate]->Draw("COLZ");
+   }
+  //txty residuals, not saved in AFF files, but I can get it directly from report canvas
+  cTXTY[whichcanvas]->cd(iplate-whichcanvas*canvasarea+1);
+  if (inputfile->GetListOfKeys()->Contains("report_al") ){
+  TCanvas *creport = (TCanvas*) inputfile->Get("report_al");
+  //from canvas to subcanvas, finally to histogram
+  TCanvas *subcanvas = (TCanvas*)((TCanvas*)creport->GetPrimitive("c"))->GetPrimitive("c_6");
+  hTXTY[iplate] = (TH2F*)subcanvas->GetPrimitive("dTXdTY");
+
+  hTXTY[iplate]->SetTitle(Form("plate %i to plate %i;dx[#mum];dy[#mum]",i,i-1));
+  cTXTY[whichcanvas]->cd(iplate-whichcanvas*canvasarea+1); //we need to go back to our canvas, otherwise we will draw in that canvas
+  hTXTY[iplate]->Draw("COLZ");
+   }
+  }
+  iplate++;
+ }
+}
+
+int global_check_align(TString runname = "",int lastplate=57, int firstplate=1){
+
+ if (runname==" "){ 
+  cout<<"Script for checking alignment. Usage: check_align(\"RUN5\",lastplate,firstplate). The check for dz needs a scanset prepared with all the plates"<<endl; 
+  return 1;
+ }
+ align_check(runname,lastplate,firstplate);
+ checkdz(runname,lastplate,firstplate);
+ return 0;
 }
