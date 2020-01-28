548be98199c54b62b6d13ee5c1314c01e3dc13ef
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2020-01-28 19:32:58 +0100

    converted from python script to add quarter information

--- analisi_charmsim/comparedistributions.C
+++ analisi_charmsim/comparedistributions.C
@@ -1,4 +1,11 @@
 //compare values for all and only reconstructed events
+/*
+ Added scripts for writing information in tree friends (DO NOT COPY THEM FROM ONE FILE TO ANOTHER),
+ add_dsresults() for saving the events numbers where it was reconstructed
+ findquarter() to get the ones in different quarter (we will need to select only the ones in second quarter)
+ comparedistributions() to produce the efficiency plots 
+
+*/
 using namespace ROOT;
 
 RVec<int> whichcharm (RVec<int> pdgcodes){
@@ -18,6 +25,20 @@ RVec<int> whichcharm (RVec<int> pdgcodes){
  return whichcharm;
 }
 
+RVec<int> decayinsecondquarter (int quarter){
+
+ if (quarter==2){ 
+   RVec<int> decaysinsecond = {1,1};
+   return decaysinsecond;
+   }
+ else{
+   RVec<int> decaysinsecond = {0,0};
+   return decaysinsecond;
+ }
+
+}
+
+
 //show subsample of distributions according to selection
 
 RVec<int> selectedintdistribution (RVec<int> distribution, RVec<int> selection){
@@ -25,10 +46,21 @@ RVec<int> selectedintdistribution (RVec<int> distribution, RVec<int> selection){
   return distribution[selection];
 
 }
+RVec<int> selectedintdistribution_2sels (RVec<int> distribution, RVec<int> selection1, RVec<int> selection2){
+  
+  return distribution[selection1*selection2];
+
+}
 
 RVec<float> selecteddistribution (RVec<float> distribution, RVec<int> selection){
   
   return distribution[selection];
+
+}
+
+RVec<float> selecteddistribution_2sels (RVec<float> distribution, RVec<int> selection1, RVec<int> selection2){
+  
+  return distribution[selection1*selection2];
 
 }
 
@@ -69,22 +101,26 @@ void plotoriginalvsreco(ROOT::RDF::RResultPtr<TH1D> horiginal, ROOT::RDF::RResul
 }
 
 void comparedistributions(){
-  TFile *file = TFile::Open("distributions_mctrue_test.root");
+  TFile *file = TFile::Open("distributions_mctrue_withquarter_withds.root");
   //connecting trees through addfriend
   TTree *mcdistributions = (TTree*) file->Get("charmdecays");
   TTree *dsresults = (TTree*) file->Get("dsrecoevents");
+  TTree *quarterstree = (TTree*) file->Get("quarters"); //we need to know which events belong to second quarter
+
   mcdistributions->AddFriend(dsresults,"ds");
+  mcdistributions->AddFriend(quarterstree,"quartersinfo");
 
   RDataFrame distdataframe = RDataFrame(*mcdistributions);
   //only the first 10000 events are actually passed to FEDRA for reconstructions
   const int nevents = 10000;
   auto dfpassedtofedra = distdataframe.Range(0,nevents);
-  auto dfdecaylength = dfpassedtofedra.Define("dl",[](RVec<float> dx, RVec<float> dy, RVec<float> dz){RVec<float>length=sqrt(dx*dx+dy*dy+dz*dz);return length;},{"dx","dy","dz"});
+  auto dfdefinedselection = dfpassedtofedra.Define("decayinsecondquarter",decayinsecondquarter,{"quarter"}); 
+  auto dfdecaylength = dfdefinedselection.Define("dl",[](RVec<float> dx, RVec<float> dy, RVec<float> dz){RVec<float>length=sqrt(dx*dx+dy*dy+dz*dz);return length;},{"dx","dy","dz"});
   auto dfcharmnames =  dfdecaylength.Define("charmtype",whichcharm,{"pdgcode"});
 
-  auto hcharmtype = dfcharmnames.Histo1D({"hcharmtype","Charmed hadron",8,0,8},"charmtype");
-  auto hcharmtype_reco = dfcharmnames.Define("charmtype_reco",selectedintdistribution,{"charmtype","reconstructed"}).Histo1D({"hcharmtypereco","Reco Charmed hadron",8,0,8},"charmtype_reco");
-  auto hcharmtype_ds = dfcharmnames.Define("charmtype_ds",selectedintdistribution,{"charmtype","dsreco"}).Histo1D({"hcharmtypeds","Ds Reco Charmed hadron",8,0,8},"charmtype_ds");
+  auto hcharmtype = dfcharmnames.Define("charmtype_inquarter",selectedintdistribution,{"charmtype","decayinsecondquarter"}).Histo1D({"hcharmtype","Charmed hadron",8,0,8},"charmtype_inquarter");
+  auto hcharmtype_reco = dfcharmnames.Define("charmtype_reco",selectedintdistribution_2sels,{"charmtype","reconstructed","decayinsecondquarter"}).Histo1D({"hcharmtypereco","Reco Charmed hadron",8,0,8},"charmtype_reco");
+  auto hcharmtype_ds = dfcharmnames.Define("charmtype_ds",selectedintdistribution_2sels,{"charmtype","dsreco","decayinsecondquarter"}).Histo1D({"hcharmtypeds","Ds Reco Charmed hadron",8,0,8},"charmtype_ds");
 
   namebins(hcharmtype);
   namebins(hcharmtype_reco);
@@ -94,24 +130,30 @@ void comparedistributions(){
 
   //lambda functions, to capture directly dataframe and apply operations for drawing
   auto compareoriginalvsreco = [&dfcharmnames] (TString columnvariable,ROOT::RDF::TH1DModel histoparameters,bool isint = false){ //for all float/double variables is ok, setisint to true for int variables
-    auto horiginal = dfcharmnames.Histo1D(histoparameters,(columnvariable).Data());
 
+    ROOT::RDF::RResultPtr<TH1D> horiginal;
     ROOT::RDF::RResultPtr<TH1D> hreco;
     ROOT::RDF::RResultPtr<TH1D> hds;
     if (!isint){ 
+     horiginal = dfcharmnames.Define((columnvariable+TString("_inquarter")).Data(),selecteddistribution,{columnvariable.Data(),"decayinsecondquarter"}).
+                                  Histo1D(histoparameters,(columnvariable+TString("_inquarter")).Data());
+
      hds = dfcharmnames.
-                          Define((columnvariable+TString("_ds")).Data(),selecteddistribution,{columnvariable.Data(),"dsreco"}).
+                          Define((columnvariable+TString("_ds")).Data(),selecteddistribution_2sels,{columnvariable.Data(),"dsreco","decayinsecondquarter"}).
                           Histo1D(histoparameters,(columnvariable+TString("_ds")).Data());
      hreco = dfcharmnames.
-                          Define((columnvariable+TString("_reco")).Data(),selecteddistribution,{columnvariable.Data(),"reconstructed"}).
+                          Define((columnvariable+TString("_reco")).Data(),selecteddistribution_2sels,{columnvariable.Data(),"reconstructed","decayinsecondquarter"}).
                           Histo1D(histoparameters,(columnvariable+TString("_reco")).Data());
     }
     else{ 
+     horiginal = dfcharmnames.Define((columnvariable+TString("_inquarter")).Data(),selectedintdistribution,{columnvariable.Data(),"decayinsecondquarter"}).
+                                  Histo1D(histoparameters,(columnvariable+TString("_inquarter")).Data());
+
      hds = dfcharmnames.
-                          Define((columnvariable+TString("_ds")).Data(),selectedintdistribution,{columnvariable.Data(),"ds.dsreco"}).
+                          Define((columnvariable+TString("_ds")).Data(),selectedintdistribution_2sels,{columnvariable.Data(),"dsreco","decayinsecondquarter"}).
                           Histo1D(histoparameters,(columnvariable+TString("_ds")).Data());     
      hreco = dfcharmnames.
-                         Define((columnvariable+TString("_reco")).Data(),selectedintdistribution,{columnvariable.Data(),"reconstructed"}).
+                         Define((columnvariable+TString("_reco")).Data(),selectedintdistribution_2sels,{columnvariable.Data(),"reconstructed","decayinsecondquarter"}).
                          Histo1D(histoparameters,(columnvariable+TString("_reco")).Data());   
     }
     TCanvas *c = new TCanvas(); //renaming reco histogram with a different name
@@ -200,3 +242,77 @@ void add_dsresults(){
 }
 
 
+
+//script to find which quarter each event belongs to
+void findquarter(){
+ TFile *simfile = TFile::Open("inECC_ship.conical.Pythia8CharmOnly-TGeant4_dig.root");
+ TTreeReader simtree("cbmsim",simfile);
+  
+ TTreeReaderArray<BoxPoint> emupoints(simtree,"BoxPoint");
+ TTreeReaderArray<BoxPoint> spreademupoints(simtree,"EmuBaseTrks"); 
+
+ //histograms
+ TH1I* hquarter = new TH1I("hquarter","which quarter",4,1,5);
+ TH2D * hxy = new TH2D("hxy","XY spread from each event;x[#mum];y[#mum]",125,0,125000,100,0,100000);
+
+ //moving target sim parameters
+ double xcut = 62500;
+ double ycut = 50000;
+
+ double spilldy = 2.0;
+ double targetmoverspeed = 2.6;
+
+ //file to update with quarter information
+
+ int quarter = 0;
+
+ TFile * outputfile = new TFile("distributions_mctrue_withquarter.root","UPDATE");
+ TTree * outputtree = new TTree("quarters","Quarters information");
+ outputtree->Branch("quarter",&quarter,"quarter/I");
+
+ const int nevents = 10000;
+ //start loop within events
+ for (int ievent = 0; ievent < nevents;ievent++){
+  simtree.SetEntry(ievent);
+  quarter = 1;
+  if (ievent%1000 == 0)
+     cout<<"arrived at "<<ievent<<endl;
+
+  double deltat = spreademupoints[0].GetTime() - emupoints[0].GetTime();
+
+  int nspill = deltat/100;
+
+  double pottime = deltat - nspill*100;
+
+  double spreadx = emupoints[0].GetX() -12.5/2. + pottime * targetmoverspeed;
+  double spready = emupoints[0].GetY() - 9.9/2. + nspill * spilldy;
+
+
+  spreadx = spreadx* 1E+4 + 62500;
+  spready = spready* 1E+4 + 49500;   
+
+  if( spreadx > xcut) quarter = quarter + 1;
+  if( spready > ycut) quarter = quarter + 2;
+   
+  if (spready < 0) quarter = quarter - 10;
+  //saving output
+   
+  hquarter->Fill(quarter);
+  hxy->Fill(spreadx,spready);
+  outputtree->Fill();
+ }
+//end loop over event
+
+TCanvas * cxy = new TCanvas();
+hxy->Draw("COLZ");
+
+for (int ibin = 0; ibin < hquarter->GetNbinsX();ibin++)
+  cout<<"In quarter "<<ibin+1<<"how many events ?"<<hquarter->GetBinContent(ibin+1)<<endl;
+
+TCanvas * cquarter = new TCanvas();
+hquarter->Draw();
+
+outputfile->cd();
+outputtree->Write();
+outputfile->Close();
+}
