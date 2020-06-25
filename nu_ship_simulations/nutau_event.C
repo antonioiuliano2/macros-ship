fcfda278cd5933507389287d02ffd50caca55f77
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2020-06-25 16:21:52 +0200

    version of first presentation

--- nu_ship_simulations/nutau_event.C
+++ nu_ship_simulations/nutau_event.C
@@ -1,22 +1,41 @@
 /*look at topology of nutau simulation for my thesis*/
-void simple_loop(){
+void nutau_event(){
  //getting tree and defining arrays
  TFile *file = TFile::Open("ship.conical.Genie-TGeant4.root"); 
  if (!file) return;
  TTreeReader reader("cbmsim",file);
 
  TTreeReaderArray<ShipMCTrack> tracks(reader,"MCTrack");
- TTreeReaderArray<PixelModulesPoint> targetpoints(reader,"TargetPoint");
+ TTreeReaderArray<TargetPoint> targetpoints(reader,"TargetPoint");
 
- TH1I *hnfilmtau = new TH1I("hnfilmtau","Number of emulsion films passed by a tau lepton;Nfilms",10,0,10);
+ TH1I *hnfilmtau = new TH1I("hnfilmtau","Number of emulsion films passed by a tau lepton;Nfilms",15,0,15);
+ TH2D *htauppt = new TH2D("htauppt","Transverse momentum vs momentum of tau lepton;P[GeV/c];Pt[GeV/c]",400,0,400,100,0,10);
+ TH1D *htaugamma = new TH1D("htaugamma","Gamma of tau lepton;#gamma",100,0,100);
+ 
  int trackID, ntauhits;
-// cout<<"Number of events"<<reader.GetEntries()<<endl;
- int ientry = 0;
- while (reader.Next()){
+ double energy, mass;
+
+ TDatabasePDG *pdg = TDatabasePDG::Instance();
+
+ cout<<"Number of events"<<reader.GetEntries()<<endl;
+ const int nentries = reader.GetEntries();
+
+ //vector<int> taudaughters;
+ for(int ientry = 0;ientry<nentries;ientry++){
+ //while (reader.Next()){
      //resetting counters
      ntauhits = 0;
-     ientry = reader.GetCurrentEntry();// keeps track of the number of event (from 0 to Nevents - 1)
+     if (ientry%10000 == 0) cout<<"arrived at entry" <<ientry<<endl;
+     reader.SetEntry(ientry);// keeps track of the number of event (from 0 to Nevents - 1)
      //if neutrino interacted off of our target, go to next
+     if ( (TMath::Abs(tracks[0].GetStartX())>40) || (TMath::Abs(tracks[0].GetStartY())>40) ) continue;
+     htauppt->Fill(tracks[1].GetP(), tracks[1].GetPt());
+
+     //computing gamma factor E/m
+     energy = tracks[1].GetEnergy();
+     mass = pdg->GetParticle(tracks[1].GetPdgCode())->Mass();
+     htaugamma->Fill(energy/mass);
+
      //access the array of tracks
 /*     for (const ShipMCTrack& track: tracks){
          if (ientry < 10) cout<<"PdgCode of the track: "<<track.GetPdgCode()<<" at event number "<<ientry<<endl;
@@ -28,8 +47,16 @@ void simple_loop(){
             ntauhits++;
         }
      } //end of hit loop
-     hnfilmtau->Fill(ntauhits);
+     hnfilmtau->Fill(ntauhits,tracks[0].GetWeight());
  } // end of event loop
  TCanvas *cnfilm = new TCanvas();
  hnfilmtau->Draw();
+
+ cout<<"Fraction of short decays"<<hnfilmtau->Integral(1,1)/hnfilmtau->Integral()<<endl;
+ 
+ TCanvas *cp = new TCanvas();
+ htauppt->Draw("COLZ");
+
+ TCanvas *cgamma = new TCanvas();
+ htaugamma->Draw();
 }
