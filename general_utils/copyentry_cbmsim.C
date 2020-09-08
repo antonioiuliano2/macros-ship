8e097ce20e9fa2cda24639bdf7c90afa7aaa4a98
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2020-09-08 15:05:01 +0200

    adding variable with list of entries to copy

--- general_utils/copyentry_cbmsim.C
+++ general_utils/copyentry_cbmsim.C
@@ -2,6 +2,8 @@
 
 void copyentry_cbmsim(){
  TFile * origfile = TFile::Open("pythia8_Geant4_1000_0.1_dig.root","READ");
+ ROOT::RVec<int> whichentries = {29584}; 
+
  TTree *origtree = (TTree*) origfile->Get("cbmsim"); 
  TList * origlist = (TList*) origfile->Get("BranchList"); //we need this list for the display too
  TList * origtimelist = (TList*) origfile->Get("TimeBasedBranchList"); //we need this list for the display too
@@ -11,10 +13,11 @@ void copyentry_cbmsim(){
 
  TFile * newfile = new TFile("selectedtree.root","RECREATE"); 
  TTree * newtree = origtree->CloneTree(0);
-
-
- origtree->GetEntry(29584);
- newtree->Fill();
+ //loop over entries to copy
+ for {int ientry::whichentries}{
+  origtree->GetEntry(ientry);
+  newtree->Fill();
+ }
 
  //saving output
  origlist->Write("BranchList",1);
