fe5ea242a792d604e9ed35527128532f3aaca3a4
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2020-10-03 12:07:40 +0200

    merging dataframes and plotting efficiency and purity together

--- analisi_charmsim/FEDRA_tracking_quality_dataframe.py
+++ analisi_charmsim/FEDRA_tracking_quality_dataframe.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import sys
-import seaborn as sns
 
 
 simdf = pd.read_csv(sys.argv[1])
@@ -14,7 +13,7 @@ nsegtrue = simdf.groupby(["MCEvent","MCTrack"]).count()
 
 
 #keeping only segments which have been connected to tracks, sorting the dataframe
-simdf = simdf.query("TrackID>=0")
+simdf = simdf.query("FEDRATrackID>=0")
 print("size of tracked segment sample ", len(simdf))
 
 simdf = simdf.sort_values(["MCEvent", "MCTrack", "PID"],ascending = [True,True,False]) #from upstream to downstream plates
@@ -24,10 +23,10 @@ def efficiency(simdf,nsegtrue):
 
  #then, require them to belong to the same reconstructed track
 
- nsegreco = simdf.groupby(["MCEvent","MCTrack","TrackID"]).count()
+ nsegreco = simdf.groupby(["MCEvent","MCTrack","FEDRATrackID","quarter"]).count()
 
  #keeping only the first segment, then adding the number of segments as new column
- simdftracks = simdf.groupby(["MCEvent","MCTrack","TrackID"]).first()
+ simdftracks = simdf.groupby(["MCEvent","MCTrack","FEDRATrackID","quarter"]).first()
  simdftracks["nsegreco"] = nsegreco["ID"]
 
  #sorting with descending nsegreco, so we keep the track with largest nsegreco
@@ -43,33 +42,47 @@ def efficiency(simdf,nsegtrue):
 def purity(simdf):
  '''for each reco track, divide number of segments belonging to the same MCTrack for the total number of reconstructed segments'''
  #first, count nseg for each reconstructed track
- nseg = simdf.groupby("TrackID").count()
- recotrackdf = simdf.groupby("TrackID").first()
+ nseg = simdf.groupby(["FEDRATrackID","quarter"]).count()
+ recotrackdf = simdf.groupby(["FEDRATrackID","quarter"]).first()
  recotrackdf["nseg"] = nseg["ID"]
 
- nsegreco = simdf.groupby(["MCEvent","MCTrack","TrackID"]).count()
+ nsegreco = simdf.groupby(["MCEvent","MCTrack","FEDRATrackID","quarter"]).count()
  #keeping only the first segment, then adding the number of segments as new column
- simdftracks = simdf.groupby(["MCEvent","MCTrack","TrackID"]).first()
+ simdftracks = simdf.groupby(["MCEvent","MCTrack","FEDRATrackID","quarter"]).first()
  simdftracks["nsegreco"] = nsegreco["ID"]
 
  #sorting with descending nsegreco, so we keep the track with largest nsegreco
  simdftracks.sort_values(["MCEvent","MCTrack","nsegreco"],ascending=[True,True,False])
  simdftracks = simdftracks.reset_index()
- simdfbesttrack = simdftracks.groupby("TrackID").first()
+ simdfbesttrack = simdftracks.groupby(["FEDRATrackID","quarter"]).first()
  simdfbesttrack["nseg"] = nseg["ID"]
  simdfbesttrack["purity"] = simdfbesttrack["nsegreco"] / simdfbesttrack["nseg"]
 
  return simdfbesttrack
 
+#obtaining and merging samples
+
 simdfbesttrack = efficiency(simdf,nsegtrue)
 simdfbesttrack2 = purity(simdf)
 
-#profile histogram, computing mean of the column for each segments
-length = np.linspace(1,29,29)
-efficiencies = []
-for l in length:
-    subset = simdfbesttrack.query("nsegtrue == {}".format(l))
-    efficiencies.append(subset.mean()["efficiency"])
+mergedsimdf = simdfbesttrack2.merge(simdfbesttrack)
 
-plt.plot(length,efficiencies,"r")
+#profile histogram, computing mean of the column for each segments
+minlength = 3 #useless count 1 and 2 segments
+length = np.linspace(minlength,29,30-minlength)
+efficiencies = []
+purities = []
+for l in length:
+    subset = mergedsimdf.query("nsegtrue == {}".format(l))
+    efficiencies.append(subset.mean()["efficiency"]*100)
+
+    subset2 = mergedsimdf.query("nsegtrue == {}".format(l)) 
+    purities.append(subset2.mean()["purity"]*100)
+
+plt.plot(length,efficiencies,"r",label="efficiency")
+plt.plot(length,purities,"b",label= "purity")
+plt.xlabel("Track length (number of segments)")
+plt.ylabel("Percentage (%)")
+plt.ylim((0, 100)) 
+plt.legend()
 plt.show()
