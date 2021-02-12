c29cd9e70fe047be997bbd6d650297f015cbda56
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2021-02-12 12:32:25 +0100

    adding efficiency and purity vs true MC momentum

--- analisi_charmsim/FEDRA_tracking_quality_dataframe.py
+++ analisi_charmsim/FEDRA_tracking_quality_dataframe.py
@@ -14,6 +14,7 @@ if ("FEDRATrackID" not in simdf.keys()):
 if ("quarter" not in simdf.keys()):
  simdf["quarter"] = 0
 
+simdf["P"] = np.sqrt(np.power(simdf["startPx"],2) + np.power(simdf["startPy"],2) + np.power(simdf["startPz"],2)) #trimomentum
 print("original size of segment sample ", len(simdf))
 nsegtrue = simdf.groupby(["MCEvent","MCTrack"]).count()
 
@@ -63,15 +64,17 @@ def purity(simdf):
  simdfbesttrack = simdftracks.groupby(["FEDRATrackID","quarter"]).first()
  simdfbesttrack["nseg"] = nseg["ID"]
  simdfbesttrack["purity"] = simdfbesttrack["nsegreco"] / simdfbesttrack["nseg"]
-
+ 
  return simdfbesttrack
 
 #obtaining and merging samples
 
 simdfbesttrack = efficiency(simdf,nsegtrue)
+print("Computed efficiency")
 simdfbesttrack2 = purity(simdf)
+print("Computed purity")
 
-mergedsimdf = simdfbesttrack2.merge(simdfbesttrack)
+#mergedsimdf = simdfbesttrack2.merge(simdfbesttrack)
 
 #profile histogram, computing mean of the column for each segments
 minlength = 3 #useless count 1 and 2 segments
@@ -83,18 +86,60 @@ efficiencies_errors = []
 purities_errors = []
 for l in length:
 
-    subset = mergedsimdf.query("nsegtrue == {}".format(l))
+    print("Adding tracks with {} segments".format(l))
+    subset = simdfbesttrack.query("nsegtrue == {}".format(l))
+    subset2 = simdfbesttrack2.query("nsegreco == {}".format(l))
     N = len(subset)
+    N2 = len(subset2)
+    
     efficiencies.append(subset.mean()["efficiency"]*100)
     efficiencies_errors.append(subset.std()["efficiency"]/np.sqrt(N)*100)
 
-    purities.append(subset.mean()["purity"]*100)
-    purities_errors.append(subset.std()["purity"]/np.sqrt(N)*100)
+    purities.append(subset2.mean()["purity"]*100)
+    purities_errors.append(subset2.std()["purity"]/np.sqrt(N2)*100)
 
-plt.errorbar(length,efficiencies,c = "r",fmt='.', yerr=efficiencies_errors,ecolor="r",label="efficiency")
-plt.errorbar(length,purities,c = "b",fmt='.',yerr=purities_errors,ecolor="b",label= "purity")
-plt.xlabel("Track length (number of segments)")
-plt.ylabel("Percentage (%)")
+#making efficiency/purity vs P
+
+efficiencies_momentum = []
+purities_momentum = []
+
+efficiencies_errors_momentum = []
+purities_errors_momentum = []
+
+minP = 0
+maxP = 400
+Pstep = 20
+Pbins = np.arange(minP, maxP, Pstep)
+
+for P in Pbins:
+    print("Adding tracks with P between {} and {}".format(P,P+Pstep))
+    
+    subset = simdfbesttrack.query("P >= {} and P < {}".format(P, P + Pstep))
+    subset2 = simdfbesttrack2.query("P >= {} and P < {}".format(P, P + Pstep))
+
+    N = len(subset)
+    N2 = len(subset2)
+    
+    efficiencies_momentum.append(subset.mean()["efficiency"]*100)
+    efficiencies_errors_momentum.append(subset.std()["efficiency"]/np.sqrt(N)*100)
+
+    purities_momentum.append(subset2.mean()["purity"]*100)
+    purities_errors_momentum.append(subset2.std()["purity"]/np.sqrt(N2)*100)
+
+#drawing figures
+plt.figure()
+plt.errorbar(length,efficiencies,c = "r",fmt='.', yerr=efficiencies_errors,ecolor="r",label="efficiency",markersize=18)
+plt.errorbar(length,purities,c = "b",fmt='.',yerr=purities_errors,ecolor="b",label= "purity",markersize=18)
+plt.xlabel("Track length (number of segments)",fontsize=20)
+plt.ylabel("Percentage (%)",fontsize=20)
 plt.ylim((0, 100)) 
-plt.legend()
+plt.legend(fontsize=20)
+
+plt.figure()
+plt.errorbar(Pbins,efficiencies_momentum,c = "r",fmt='.', yerr=efficiencies_errors_momentum,ecolor="r",label="efficiency",markersize=18)
+plt.errorbar(Pbins,purities_momentum,c = "b",fmt='.',yerr=purities_errors_momentum,ecolor="b",label= "purity",markersize=18)
+plt.xlabel("Momentum [GeV/c]",fontsize=20)
+plt.ylabel("Percentage (%)",fontsize=20)
+plt.ylim((0, 100)) 
+plt.legend(fontsize=20)
 plt.show()
