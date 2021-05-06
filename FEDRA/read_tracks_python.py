de53d52ec45c95351be44382973b8cc7a0630847
Antonio Iuliano <antonio.iuliano1993@gmail.com>
2021-05-06 11:43:19 +0200

    adding instructions to access covariance matrix

--- FEDRA/read_tracks_python.py
+++ FEDRA/read_tracks_python.py
@@ -1,20 +1,22 @@
 import ROOT as r
 import rootlogon
 c = r.TCanvas()
-def standard_read():
-	#opening the file
-	f = r.TFile.Open("linked_tracks.root")
-	tree = f.Get('tracks')
 
-	tree.SetAlias("t","t.") #setting the alias for the track branch
+#opening the file
+f = r.TFile.Open("linked_tracks.root")
+tree = f.Get('tracks')
+tree.SetAlias("t","t.") #setting the alias for the track branch
+#for i in range(tree.GetEntries()): 
+tree.GetEntry(0)
+track = tree.t
+segments = tree.s
+fittedsegments = tree.sf
 
-	#for i in range(tree.GetEntries()): 
-	tree.GetEntry(0)
-	track = tree.t
-	segments = tree.s
+print ('Track TX angle:', track.TX(), "Segment 0 position", segments[0].X(), segments[0].Y(), segments[0].Z())
 
-	print ('Track TX angle:', track.TX(), "Segment 0 position", segments[0].X(), segments[0].Y(), segments[0].Z())
+for (seg, segf) in zip(segments, fittedsegments):
+ #we need to access Covariance matrix
+ mycov = segf.COV()
+ mycov.Print()
 
-df = r.RDataFrame('tracks','linked_tracks.root')
-histo = df.Histo1D(('hy','Y positions', 100, 0, 100000), 't.eY')
-histo.Draw()
+
