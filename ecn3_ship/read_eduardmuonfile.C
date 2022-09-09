f7b2e2c0cbd751238a3684d3a8b55644b8703afb
Antonio Iuliano <antonio.iuliano1993@gmail.com>
2022-09-09 12:33:23 +0200

    setting titles

--- ecn3_ship/read_eduardmuonfile.C
+++ ecn3_ship/read_eduardmuonfile.C
@@ -4,14 +4,26 @@ void read_eduardmuonfile(){
     TFile *inputfile = TFile::Open("root:://eosuser.cern.ch//eos/user/e/edursov/ship_data/snd_ship_ecn3/muons_3_planes.root");
     TTree *cbmsim = (TTree*) inputfile->Get("cbmsim");
     cout<<"Reading a tree with "<<cbmsim->GetEntries()<<endl;
+
     TCanvas *cxy = new TCanvas();
     cbmsim->Draw("sco_1Point.fY:sco_1Point.fX>>hxy(140,-600,800,140,-600,800)","MCTrack[0].fW","COLZ");
+    TH2D *hxy = (TH2D*) gDirectory->Get("hxy");
+    hxy->SetTitle("xy in scoring plane 1;x[cm];y[cm]");
+    
+
     TCanvas *cxy_zoomed = new TCanvas();
     cbmsim->Draw("sco_1Point.fY:sco_1Point.fX>>hxy_zoomed(400,-200,200,400,-200,200)","MCTrack[0].fW","COLZ");
+    TH2D *hxy_zoomed = (TH2D*) gDirectory->Get("hxy_zoomed");
+    hxy_zoomed->SetTitle("xy zoomed in scoring plane 1;x[cm];y[cm]");
 
     TCanvas *cP = new TCanvas();
-    cbmsim->Draw("TMath::Sqrt(sco_1Point.fPx*sco_1Point.fPx+sco_1Point.fPy*sco_1Point.fPy+sco_1Point.fPz*sco_1Point.fPz)","MCTrack[0].fW");
+    cbmsim->Draw("TMath::Sqrt(sco_1Point.fPx*sco_1Point.fPx+sco_1Point.fPy*sco_1Point.fPy+sco_1Point.fPz*sco_1Point.fPz)>>hP","MCTrack[0].fW");
+    TH1D *hP = (TH1D*) gDirectory->Get("hP");
+    hP->SetTitle("momentum in scoring plane 1;P[GeV/c]");
 
     TCanvas *cpdgcode = new TCanvas();
-    cbmsim->Draw("sco_1Point.fPdgCode","MCTrack[0].fW");
+    cbmsim->Draw("sco_1Point.fPdgCode>>hPdgCode","MCTrack[0].fW");
+    TH1D *hPdgCode = (TH1D*) gDirectory->Get("hPdgCode");
+    hPdgCode->SetTitle("PdgCode in scoring plane 1;PdgCode");
+    
 }
