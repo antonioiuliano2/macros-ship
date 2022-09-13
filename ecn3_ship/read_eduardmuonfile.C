9697c637b0aaa3278ab647d05e29e737b32b50af
Antonio Iuliano <antonio.iuliano1993@gmail.com>
2022-09-13 18:14:54 +0200

    weighting the dataframe, tagging the upstream

--- ecn3_ship/read_eduardmuonfile.C
+++ ecn3_ship/read_eduardmuonfile.C
@@ -1,29 +1,70 @@
 using namespace ROOT;
+
+RVec<double> GetX(TClonesArray &pointarray){
+    RVec<double> myx;
+    const int npoints = pointarray.GetEntries();
+    for (int i=0; i < npoints;i++){
+        FairMCPoint *point = (FairMCPoint*) pointarray.At(i);
+        myx.push_back(point->GetX());
+    }
+    return myx;
+}
+
+RVec<double> GetY(TClonesArray &pointarray){
+    RVec<double> myy;
+    const int npoints = pointarray.GetEntries();
+    for (int i=0; i < npoints;i++){
+        FairMCPoint *point = (FairMCPoint*) pointarray.At(i);
+        myy.push_back(point->GetY());
+    }
+    return myy;
+}
+
+RVec<double> GetWeight(TClonesArray &trackarray,TClonesArray &pointarray){
+
+    RVec<double> sco1_weights;
+    const int npoints = pointarray.GetEntries();
+    for (int i=0; i < npoints;i++){
+        FairMCPoint *point = (FairMCPoint*) pointarray.At(i);
+        //get corresponding track
+        ShipMCTrack* mctrack = (ShipMCTrack*) trackarray.At(point->GetTrackID());
+        sco1_weights.push_back(mctrack->GetWeight());
+    }
+
+    
+    return sco1_weights;
+}
+
 void read_eduardmuonfile(){
+
+    EnableImplicitMT(); //we need to go parallel
     //first, accessing the file from EOS
-    TFile *inputfile = TFile::Open("root:://eosuser.cern.ch//eos/user/e/edursov/ship_data/snd_ship_ecn3/muons_3_planes.root");
-    TTree *cbmsim = (TTree*) inputfile->Get("cbmsim");
-    cout<<"Reading a tree with "<<cbmsim->GetEntries()<<endl;
+    //TFile *inputfile = TFile::Open("root:://eosuser.cern.ch//eos/user/e/edursov/ship_data/snd_ship_ecn3/muons_3_planes.root");
+    TFile *inputfile = TFile::Open("root:://eosuser.cern.ch//eos/user/e/edursov/ship_data/ntuples_for_dis/combi_ecn3/merged_combi_ecn3_spill_11.root");
+    //build rdataframe
+    RDataFrame df("cbmsim",inputfile);
 
-    TCanvas *cxy = new TCanvas();
-    cbmsim->Draw("sco_1Point.fY:sco_1Point.fX>>hxy(140,-600,800,140,-600,800)","MCTrack[0].fW","COLZ");
-    TH2D *hxy = (TH2D*) gDirectory->Get("hxy");
-    hxy->SetTitle("xy in scoring plane 1;x[cm];y[cm]");
-    
+    //const char* detname = "sco_1Point";
+    const char* detname = "UpstreamTaggerPoint";
+    auto df1 = df.Define("sco1X",GetX,{detname})
+                .Define("sco1Y",GetY,{detname})
+                .Define("sco1Weight",GetWeight,{"MCTrack",detname});
 
-    TCanvas *cxy_zoomed = new TCanvas();
-    cbmsim->Draw("sco_1Point.fY:sco_1Point.fX>>hxy_zoomed(400,-200,200,400,-200,200)","MCTrack[0].fW","COLZ");
-    TH2D *hxy_zoomed = (TH2D*) gDirectory->Get("hxy_zoomed");
-    hxy_zoomed->SetTitle("xy zoomed in scoring plane 1;x[cm];y[cm]");
+    cout<<"Starting rdataframe analysis"<<endl;
+    auto hxy = df1.Histo2D({"hxy","xy distribution of muons;x[cm];y[cm]",140,-600,800,140,-600,800},"sco1X","sco1Y","sco1Weight");
+    auto hxy_zoomed = df1.Histo2D({"hxy_zoomed","xy distribution of muons;x[cm];y[cm]",400,-200,200,400,-200,200},"sco1X","sco1Y","sco1Weight");
+    auto hxy_zoomed_text = df1.Histo2D({"hxy_zoomed_text","xy distribution of muons;x[cm];y[cm]",40,-200,200,40,-200,200},"sco1X","sco1Y","sco1Weight");
 
-    TCanvas *cP = new TCanvas();
-    cbmsim->Draw("TMath::Sqrt(sco_1Point.fPx*sco_1Point.fPx+sco_1Point.fPy*sco_1Point.fPy+sco_1Point.fPz*sco_1Point.fPz)>>hP","MCTrack[0].fW");
-    TH1D *hP = (TH1D*) gDirectory->Get("hP");
-    hP->SetTitle("momentum in scoring plane 1;P[GeV/c]");
+    TFile *outputfile = new TFile("plots_muonshitseduard_upstreamBIG.root","RECREATE");
+    TCanvas *cxy = new TCanvas("cxy","xy distribution",800,800);
+    hxy->DrawClone("COLZ");
+    cxy->Write();
+    TCanvas *cxy_zoomed = new TCanvas("cxy_zoomed","xy distribution",800,800);
+    hxy_zoomed->DrawClone("COLZ");
+    cxy_zoomed->Write();
+    TCanvas *cxy_zoomed_text = new TCanvas("cxy_zoomed_text","xy distribution with text in bins",800,800);
+    hxy_zoomed_text->DrawClone("COLZ&&TEXT");    
+    cxy_zoomed_text->Write();
 
-    TCanvas *cpdgcode = new TCanvas();
-    cbmsim->Draw("sco_1Point.fPdgCode>>hPdgCode","MCTrack[0].fW");
-    TH1D *hPdgCode = (TH1D*) gDirectory->Get("hPdgCode");
-    hPdgCode->SetTitle("PdgCode in scoring plane 1;PdgCode");
-    
+
 }
