819425f8045f90d6b86d1c2e9586f85c9e614286
Antonio Iuliano <antonio.iuliano1993@gmail.com>
2023-08-07 15:45:42 +0200

    better formatting and adding charm case

--- ecn3_ship/getnuestimations.C
+++ ecn3_ship/getnuestimations.C
@@ -2,14 +2,14 @@
 void getnuresults(){
 
  double normsim = 5e+13; //reference of simulation weights (aka. POT for one spill)
- double normship = 4e+19; //reference for five years of SND DataTaking
+ double normship =6e+20; //replace to have multiple years of data taking
 
- TFile *nuefile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots/results_nu_e_dis_cc.root");
- TFile *numufile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots/results_nu_mu_dis_cc.root");
- TFile *nutaufile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots/results_nu_tau_dis_cc.root");
- TFile *nuebarfile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots/results_nu_e_bar_dis_cc.root");
- TFile *numubarfile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots/results_nu_mu_bar_dis_cc.root");
- TFile *nutaubarfile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots/results_nu_tau_bar_dis_cc.root");
+ TFile *nuefile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots_2/results_nu_e_dis_cc.root");
+ TFile *numufile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots_2/results_nu_mu_dis_cc.root");
+ TFile *nutaufile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots_2/results_nu_tau_dis_cc.root");
+ TFile *nuebarfile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots_2/results_nu_e_bar_dis_cc.root");
+ TFile *numubarfile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots_2/results_nu_mu_bar_dis_cc.root");
+ TFile *nutaubarfile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots_2/results_nu_tau_bar_dis_cc.root");
 
 
  TH1D *nuespectrum = (TH1D*) nuefile->Get("hspectrum_nu_e_intdis_cc");
@@ -20,12 +20,12 @@ void getnuresults(){
  numuspectrum->Scale(normship/normsim);
  nutauspectrum->Scale(normship/normsim);
 
-//printing yields to screen
- cout<<"N nue"<<nuespectrum->Integral()<<endl;
+ //printing yields to screen
+ cout<<"nue: Mean energy:\t"<<nuespectrum->GetMean()<<" N:\t"<<nuespectrum->Integral()<<endl;
  cout<<endl;
- cout<<"N numu"<<numuspectrum->Integral()<<endl;
+ cout<<"numu: Mean energy:\t"<<numuspectrum->GetMean()<<" N:\t"<<numuspectrum->Integral()<<endl;
  cout<<endl;
- cout<<"N nutau"<<nutauspectrum->Integral()<<endl;
+ cout<<"nutau: Mean energy:\t"<<nutauspectrum->GetMean()<<" N:\t"<<nutauspectrum->Integral()<<endl;
  cout<<endl;
 
  TH1D *nuebarspectrum = (TH1D*) nuebarfile->Get("hspectrum_nu_e_bar_intdis_cc");
@@ -37,11 +37,72 @@ void getnuresults(){
  nutaubarspectrum->Scale(normship/normsim);
 
  //printing yields to screen
- cout<<"N nuebar"<<nuebarspectrum->Integral()<<endl;
+ cout<<"nuebar: Mean energy:\t"<<nuebarspectrum->GetMean()<<" N:\t"<<nuebarspectrum->Integral()<<endl;
  cout<<endl;
- cout<<"N numubar"<<numubarspectrum->Integral()<<endl;
+ cout<<"numubar: Mean energy:\t"<<numubarspectrum->GetMean()<<" N:\t"<<numubarspectrum->Integral()<<endl;
  cout<<endl;
- cout<<"N nutaubar"<<nutaubarspectrum->Integral()<<endl;
+ cout<<"nutaubar: Mean energy:\t"<<nutaubarspectrum->GetMean()<<" N:\t"<<nutaubarspectrum->Integral()<<endl;
+ cout<<endl;
+
+ //combining nu and antinu together
+ nuespectrum->Add(nuebarspectrum);
+ numuspectrum->Add(numubarspectrum);
+ nutauspectrum->Add(nutaubarspectrum);
+
+ TCanvas *cnu = new TCanvas();
+ numuspectrum->SetLineColor(kRed);
+ numuspectrum->Draw("hist");
+ nuespectrum->SetLineColor(kBlue);
+ nuespectrum->Draw("hist&&SAMES");
+ nutauspectrum->SetLineColor(kGreen);
+ nutauspectrum->Draw("hist&&SAMES");
+
+
+}
+
+void getnucharmresults(){
+
+ double normsim = 5e+13; //reference of simulation weights (aka. POT for one spill)
+ double normship =6e+20; //replace to have multiple years of data taking
+
+ TFile *nuefile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots_2/results_nu_e_dis_cc_charm.root");
+ TFile *numufile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots_2/results_nu_mu_dis_cc_charm.root");
+ TFile *nutaufile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots_2/results_nu_tau_dis_cc_charm.root");
+ TFile *nuebarfile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots_2/results_nu_e_bar_dis_cc_charm.root");
+ TFile *numubarfile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots_2/results_nu_mu_bar_dis_cc_charm.root");
+ TFile *nutaubarfile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/plots_2/results_nu_tau_bar_dis_cc_charm.root");
+
+
+ TH1D *nuespectrum = (TH1D*) nuefile->Get("hspectrum_nu_e_intdis_cc_charm");
+ TH1D *numuspectrum = (TH1D*) numufile->Get("hspectrum_nu_mu_intdis_cc_charm");
+ TH1D *nutauspectrum = (TH1D*) nutaufile->Get("hspectrum_nu_tau_intdis_cc_charm");
+
+ nuespectrum->Scale(normship/normsim);
+ numuspectrum->Scale(normship/normsim);
+ nutauspectrum->Scale(normship/normsim);
+
+ //printing yields to screen
+ cout<<"nue: Mean energy:\t"<<nuespectrum->GetMean()<<" N:\t"<<nuespectrum->Integral()<<endl;
+ cout<<endl;
+ cout<<"numu: Mean energy:\t"<<numuspectrum->GetMean()<<" N:\t"<<numuspectrum->Integral()<<endl;
+ cout<<endl;
+ cout<<"nutau: Mean energy:\t"<<nutauspectrum->GetMean()<<" N:\t"<<nutauspectrum->Integral()<<endl;
+ cout<<endl;
+
+ TH1D *nuebarspectrum = (TH1D*) nuebarfile->Get("hspectrum_nu_e_bar_intdis_cc_charm");
+ TH1D *numubarspectrum = (TH1D*) numubarfile->Get("hspectrum_nu_mu_bar_intdis_cc_charm");
+ TH1D *nutaubarspectrum = (TH1D*) nutaubarfile->Get("hspectrum_nu_tau_bar_intdis_cc_charm");
+
+ nuebarspectrum->Scale(normship/normsim);
+ numubarspectrum->Scale(normship/normsim);
+ nutaubarspectrum->Scale(normship/normsim);
+
+ //printing yields to screen
+ cout<<"nuebar: Mean energy:\t"<<nuebarspectrum->GetMean()<<" N:\t"<<nuebarspectrum->Integral()<<endl;
+ cout<<endl;
+ cout<<"numubar: Mean energy:\t"<<numubarspectrum->GetMean()<<" N:\t"<<numubarspectrum->Integral()<<endl;
+ cout<<endl;
+ cout<<"nutaubar: Mean energy:\t"<<nutaubarspectrum->GetMean()<<" N:\t"<<nutaubarspectrum->Integral()<<endl;
  cout<<endl;
 
  //combining nu and antinu together
@@ -62,7 +123,7 @@ void getnuresults(){
 
 void getnuincomingresults(){
  double normsim = 5e+13; //reference of simulation weights (aka. POT for one spill)
- double normship = 4e+19; //reference for one year of SND DataTaking
+ double normship = 5e+13; //replace to have multiple years of data taking
 
  TFile *nufile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/neutrinos_detector.root");
 
@@ -74,11 +135,11 @@ void getnuincomingresults(){
  numuspectrum->Scale(normship/normsim);
  nutauspectrum->Scale(normship/normsim);
  //printing yields to screen
- cout<<"N nue"<<nuespectrum->Integral()<<endl;
+ cout<<"nue: Mean energy<<"<<nuespectrum->GetMean()<<" N:"<<nuespectrum->Integral()<<endl;
  cout<<endl;
- cout<<"N numu"<<numuspectrum->Integral()<<endl;
+ cout<<"numu: Mean energy<<"<<numuspectrum->GetMean()<<" N:"<<numuspectrum->Integral()<<endl;
  cout<<endl;
- cout<<"N nutau"<<nutauspectrum->Integral()<<endl;
+ cout<<"nutau: Mean energy<<"<<nutauspectrum->GetMean()<<" N:"<<nutauspectrum->Integral()<<endl;
  cout<<endl;
 
  TH1D *nuebarspectrum = (TH1D*) nufile->Get("hnu_e_bar");
@@ -91,11 +152,11 @@ void getnuincomingresults(){
 
 
  //printing yields to screen
- cout<<"N nuebar"<<nuebarspectrum->Integral()<<endl;
+ cout<<"nuebar: Mean energy:\t"<<nuebarspectrum->GetMean()<<" N:\t"<<nuebarspectrum->Integral()<<endl;
  cout<<endl;
- cout<<"N numubar"<<numubarspectrum->Integral()<<endl;
+ cout<<"numubar: Mean energy:\t"<<numubarspectrum->GetMean()<<" N:\t"<<numubarspectrum->Integral()<<endl;
  cout<<endl;
- cout<<"N nutaubar"<<nutaubarspectrum->Integral()<<endl;
+ cout<<"nutaubar: Mean energy:\t"<<nutaubarspectrum->GetMean()<<" N:\t"<<nutaubarspectrum->Integral()<<endl;
  cout<<endl;
 
  //combining nu and antinu together
@@ -115,7 +176,7 @@ void getnuincomingresults(){
 
 void getnuproducedresults(){
  double normsim = 5e+13; //reference of simulation weights (aka. POT for one spill)
- double normship = 4e+19; //reference for one year of SND DataTaking
+ double normship = 6e+20;//replace to have multiple years of data taking
 
  TFile *nufile = TFile::Open("/home/utente/Simulations/nuyield_shipecn3/25m/pythia8_Geant4_1.0_withCharm_nu.root");
 
@@ -127,11 +188,11 @@ void getnuproducedresults(){
  numuspectrum->Scale(normship/normsim);
  nutauspectrum->Scale(normship/normsim);
  //printing yields to screen
- cout<<"N nue"<<nuespectrum->Integral()<<endl;
+ cout<<"nue: Mean energy:\t"<<nuespectrum->GetMean()<<" N:\t"<<nuespectrum->Integral()<<endl;
  cout<<endl;
- cout<<"N numu"<<numuspectrum->Integral()<<endl;
+ cout<<"numu: Mean energy:\t"<<numuspectrum->GetMean()<<" N:\t"<<numuspectrum->Integral()<<endl;
  cout<<endl;
- cout<<"N nutau"<<nutauspectrum->Integral()<<endl;
+ cout<<"nutau: Mean energy:\t"<<nutauspectrum->GetMean()<<" N:\t"<<nutauspectrum->Integral()<<endl;
  cout<<endl;
 
  TH1D *nuebarspectrum = (TH1D*) nufile->Get("2012");
@@ -144,11 +205,11 @@ void getnuproducedresults(){
 
 
  //printing yields to screen
- cout<<"N nuebar"<<nuebarspectrum->Integral()<<endl;
+ cout<<"nuebar: Mean energy:\t"<<nuebarspectrum->GetMean()<<" N:\t"<<nuebarspectrum->Integral()<<endl;
  cout<<endl;
- cout<<"N numubar"<<numubarspectrum->Integral()<<endl;
+ cout<<"numubar: Mean energy:\t"<<numubarspectrum->GetMean()<<" N:\t"<<numubarspectrum->Integral()<<endl;
  cout<<endl;
- cout<<"N nutaubar"<<nutaubarspectrum->Integral()<<endl;
+ cout<<"nutaubar: Mean energy:\t"<<nutaubarspectrum->GetMean()<<" N:\t"<<nutaubarspectrum->Integral()<<endl;
  cout<<endl;
 
  //combining nu and antinu together
@@ -162,12 +223,13 @@ void getnuproducedresults(){
 
  TCanvas *cnu = new TCanvas();
  numuspectrum->SetLineColor(kRed);
- numuspectrum->Draw("hist");
+ numuspectrum->Draw();
  nuespectrum->SetLineColor(kBlue);
- nuespectrum->Draw("hist&&SAMES");
+ nuespectrum->Draw("SAMES");
  nutauspectrum->SetLineColor(kGreen);
- nutauspectrum->Draw("hist&&SAMES");
+ nutauspectrum->Draw("SAMES");
 
+ //numuspectrum->GetYaxis()->SetRangeUser(1e+3,3e+10);
  cnu->BuildLegend();
 
 }
