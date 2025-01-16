47519a110ce6cfdae4c3a37b1e7a60ab79eb2e2d
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2025-01-16 09:30:10 +0100

    plotting xy distribution

--- muon_background/read_pickle_data.py
+++ muon_background/read_pickle_data.py
@@ -4,7 +4,7 @@ import numpy as np
 import ROOT as r
 import os
 import matplotlib.pyplot as plt
-files_dir = '/eos/user/l/lcattela/muons/files_middle' #Directory with files
+files_dir = '/eos/user/l/lcattela/muons/files_old' #Directory with files
 def get_data(file):
     with gzip.open(file,'rb') as f:
         data = pickle.load(f)
@@ -20,6 +20,7 @@ nbins = 120
 total_entries = 0
 total_W = 0
 
+hxy = r.TH2D("hxy","check muon rates;x[m];y[m]", 40,-0.2,0.2,40,-0.2,0.2)
 
 totalpcounts = np.zeros(400,dtype=int)
 totalcounts = np.zeros(nbins,dtype = int) #we cannot concatenate all data due to memory, so we bin them for each file
@@ -28,6 +29,9 @@ for f in os.listdir(files_dir):
     print("File name", f)
     print("Numer entries", get_data(files_dir+"/"+f).shape[1])
     px,py,pz,x,y,z,particle,W = get_data(files_dir+"/"+f)
+    
+    for x_i, y_i, W_i in zip( x, y, W):
+     hxy.Fill(x_i, y_i, W_i)
     
     total_entries = total_entries + get_data(files_dir+"/"+f).shape[1]
     total_W = total_W + np.sum(W)
@@ -49,6 +53,9 @@ for f in os.listdir(files_dir):
 #Pt = np.sqrt(px**2+py**2)
 #P = np.sqrt(Pt**2+pz**2)
 #plotting Ecm
+
+cxy = r.TCanvas()
+hxy.Draw("COLZ")
 
 print("Total entries", total_entries)
 print("Total weight", total_W)
